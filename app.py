6320a1aef442806b7a927c9d1cb5636621acfa14
Diana Chistyakova <103204655+Shirayuky@users.noreply.github.com>
2022-11-18 01:18:29 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+import dotenv
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+dotenv.load_dotenv(override=True)
+
+@app.route('/')
+def environment_page():
+    if os.environ.get('APP_CONFIG') == 'development':
+        # то указываем путь
+        app.config.from_pyfile('config/development.py')
+        title = os.environ.get('TITLE')
+        description = os.environ.get('DESCRIPTION')
+        return f"<p>{title} - {description}</p>"
+    else:
+        app.config.from_pyfile('config/production.py')
+        title = os.environ.get('TITLE')
+        description = os.environ.get('DESCRIPTION')
+        return f"<p>{title} - {description}</p>"
+
+
+app.run()
